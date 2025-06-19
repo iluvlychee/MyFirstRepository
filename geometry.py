06facc2b80f2881283dc7a59dbe4333ae19bc87a
iluvlychee <emilia.tuazon@gmail.com>
2025-06-19 21:35:20 +0700

    my first geometry to calculate squares and rectangles

---
+++ geometry.py
@@ -0,0 +1,12 @@
+a = int(input("Enter one side of the square: "))
+b = a * a
+c = a * 4
+print("The area of the square is: ", b)
+print("The perimeter of the square is: ", c)
+
+d = int(input("Enter the width of the rectangle: "))
+e = int(input("Enter the length of the rectangle: "))
+f = d * e
+g = d + d + e + e
+print("The area of the rectangle is: ", f)
+print("The perimeter of the rectangle is: ", g)
--- input_output.py
+++ input_output.py
@@ -1,2 +1,16 @@
-x = input("Enter a name: ")
-print("Hello,", x, "welcome to the world of Python")
+# x = input("Enter a name: ")
+# print("Hello,", x, "welcome to the world of Python")
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print("The sum of", a, "and", b, "is:", c)
+print("The difference of", a, "and", b, "is:", d)
+print("The product of", a, "and", b, "is:", e)
+print("The quotient of", a, "and", b, "is:", f)
+
+# "int(__)" is used to convert the input string to an integer.
+# "input(__)" is used to take input from the user.
